ef254a9f6261d44e5982ba4a5729b341d118af12
Thomas Hagebols <thomasHagebols@users.noreply.github.com>
2017-05-22 12:14:38 +0200

    db connect script with readme update

--- README.md
+++ README.md
@@ -4,12 +4,21 @@ A family tree of the Noord-Brabant region
 The passwords of the connectors are stored in the config.py file. This file is included on the gitignore so you should add it yoursels by copying the snippet and replacing password and ip adress with the correct password and ip.
 
 ```python
-def ssh_password():
+def username():
+    username = "username"
+    return username
+
+def password():
     password = "password"
     return password
-    
+
 def ip():
     address = "ip address"
     return address
+
+def port():
+    port = int[port]
+    return port    
 ```
 
+You can connect to the mongodb using mongoconnect.py
--- mongoconnect.py
+++ mongoconnect.py
@@ -1,5 +1,39 @@
 # -*- coding: utf-8 -*-
-
+from config import *
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://tweetAdmin:wir&dmmongol@95.85.1.29:27017/')
+# Connect to the database
+def mongo_connect():
+    client = MongoClient('mongodb://' + username() + ":" + password() + "@"  + ip() + ":" + str(port()) + '/')
+    bhic = client['bhic-databases']
+
+    # Load collections
+    births = bhic['civil-status-births-certificates']
+    baptisms = bhic['dtb-baptisms-certificates']
+    marriage_acts = bhic['civil-status-marriage-acts']
+    marriage_actions = bhic['dtb-marriage-actions']
+    deaths = bhic['civil-status-deaths']
+    death_actions = bhic['dtb-death-actions']
+    succession = bhic['memories-of-succession']
+    pop_resisters = bhic['genealogical-population-registers']
+    military = bhic['military-register']
+    prison = bhic['prision-register']
+    people = bhic['people']
+
+    # Dict containing all collections of the original data source (as imported)
+    source_collections = {'births':births, 'baptisms':baptisms,
+    'marriage_acts':marriage_acts, 'marriage_actions':marriage_actions,
+    'deaths':deaths, 'death_actions':death_actions, 'succession':succession,
+    'pop_resisters':pop_resisters, 'military':military, 'prison':prison}
+
+    return client, bhic, source_collections, people
+
+if __name__ == "__main__":
+    client, bhic, source_collections, people = mongo_connect()
+
+    # test by counting # records in each collection
+    for collection in source_collections:
+        # print(type(collection))
+        print("# records in", collection, source_collections[collection].count())
+
+    print("# records in people", people.count())
