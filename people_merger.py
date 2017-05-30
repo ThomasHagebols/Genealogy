6ea740f89c4c2904e7248ca920df3f41c1ea14f7
TeunG <t.l.f.graafmans@student.tue.nl>
2017-05-31 00:49:22 +0200

    Added query builder for PersonName + DateOfBirth

---
+++ people_merger.py
@@ -0,0 +1,51 @@
+from db_connect import mongo_connect
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
+
+def merge_people():
+    mc = mongo_connect()
+
+    for person in mc['people'].find({}):
+
+        #start with an empty query
+        query = {}
+
+        #CheckLastName becomes None if 'PersonNameLastName' does not exist
+        CheckLastName = person['PersonName'].get('PersonNameLastName')
+
+        #If 'PersonNameLastName' exists
+        if CheckLastName != None:
+
+            #add LastName to the query
+            LastName = person['PersonName']['PersonNameLastName']
+            query['PersonName.PersonNameLastName'] = LastName
+
+        #CheckFirstName becomes None if 'PersonNameFirstName' does not exist
+        CheckFirstName = person['PersonName'].get('PersonNameFirstName')
+
+        #If 'PersonNameFirstName' exists
+        if CheckFirstName != None:
+
+            #add FirstName to the query
+            FirstName = person['PersonName']['PersonNameFirstName']
+            query['PersonName.PersonNameFirstName'] = FirstName
+
+        #CheckBirthDate becomes None if 'BirthDate' does not exist
+        CheckBirthDate = person.get('BirthDate')
+
+        #If 'BirthDate' exists
+        if CheckBirthDate != None:
+            BirthYear = person['BirthDate']['Year']
+            query['BirthDate.Year'] = BirthYear
+
+            #add BirthMonth to the query
+            BirthMonth = person['BirthDate']['Month']
+            query['BirthDate.Month'] = BirthMonth
+
+            BirthDay = person['BirthDate']['Day']
+            query['BirthDate.Day'] = BirthDay
+
+        print(query)
+
+merge_people()
